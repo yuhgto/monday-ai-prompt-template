666f77df863011c9aa30d118bd2fdd2a97c611e9
Dipro Bhowmik <dipro.bhowmik@gmail.com>
2023-06-08 13:54:43 -0400

    fix: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,7 @@ import BasePromptLayout from '@/examples/basic-prompt-layout/prompt-layout';
 import PromptWithColumnMapping from '@/examples/prompt-with-column-mapping/prompt-with-column-mapping';
 import LivestreamExampleFinal from '@/examples/livestream-example/final-code';
 import LivestreamExample from '@/examples/livestream-example/boilerplate';
-
-const appConfigService = {
-  systemMessage: () => {return 'hello'},
-  assistantMessagesAction: () => {return undefined},
-}
-
-const inputPlaceholder = () => {return 'hello'};
-
-const assistantAction = () => {return undefined};
+import AiAppFooter from '@/components/ai-footer/ai-footer';
 
 export default function Home() {
   return (
