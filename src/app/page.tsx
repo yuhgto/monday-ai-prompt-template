56f63edad71f02c725361fa62e3a0d8bf7dcbcab
shay <shay.elkana@gmail.com>
2023-06-18 19:52:13 +0300

    feature:Example selection page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import {useCallback, useState, useMemo} from "react";
 import Image from 'next/image'
 import styles from './page.module.css'
 import { AppContextProvider } from '@/components/context-provider/app-context-provider';
@@ -10,13 +11,55 @@ import AiAppFooter from '@/components/ai-footer/ai-footer';
 import ContextExplorerExample from '@/examples/context-explorer/context-explorer-example'
 
 export default function Home() {
+  const  [displayedApp, setAppToDisplay] = useState('');
+
+  const renderApp = useMemo(() => {
+    switch(displayedApp){
+      case 'ContextExplorerExample':
+        return (<ContextExplorerExample />);
+      case 'LivestreamExample':
+        return (<LivestreamExampleFinal />);
+      case 'BasePromptLayout':
+        return (<BasePromptLayout />);
+      default:
+        return (
+            <div>
+              <table>
+                <thead>
+                <tr>
+                  <th colSpan={3}>
+                    Please click on one of the examples below.<br/>
+                    note that you would have to reopen the app to return to this choice screen.
+                  </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td><button onClick={() => renderExampleApp('ContextExplorerExample')}>Context Explorer Example</button></td>
+                </tr>
+                <tr>
+                  <td><button onClick={() => renderExampleApp('LivestreamExample')}>Livestream Example</button></td>
+                </tr>
+                <tr>
+                  <td><button onClick={() => renderExampleApp('BasePromptLayout')}>Base Prompt Layout</button></td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+        );
+    }
+  }, [displayedApp]);
+  const renderExampleApp = (type= 'ContextExplorerExample') =>{
+    setAppToDisplay(type);
+  }
   return (
-    <div className={styles.App}>
-    <AppContextProvider>
-      <ContextExplorerExample />
-      {/* <LivestreamExampleFinal /> */}
-      {/* <BasePromptLayout /> */}
-    </AppContextProvider>
-    </div>
+      <div className={styles.App}>
+        <AppContextProvider>
+          <>
+            {renderApp}
+          </>
+        </AppContextProvider>
+      </div>
   )
 }
+
